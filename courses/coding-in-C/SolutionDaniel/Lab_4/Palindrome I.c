8dc0f7a343e6c3b01c8f485ff0df54763782a9b2
daniel.ham <hamburger.danie-it25@it.dhbw-ravensburg.de>
2026-02-06 18:54:17 +0100

    Palindrom I fixed

--- courses/coding-in-C/SolutionDaniel/Lab_4/Palindrome I.c
+++ courses/coding-in-C/SolutionDaniel/Lab_4/Palindrome I.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 
+void changeSecondLetter(char *text){
+    text[1] = 'c';
+}
+
 int main(){
-char content[100] ="Curly bird catches the worm";
-char* Var1 = &content[0];
-char* Var2 = &content[1];
 
-printf("First: %s \n", content);
-printf("Second: %c \n", *Var1);
+    char content[100] = "Curly bird catches the worm";
 
-*Var1='E';
-*Var2='a';
-
-printf("After: %s \n", content);
+    char* Var1 = &content[0];
+    char* Var2 = &content[1];
 
 
+    printf("First: %s \n", content);
+    printf("Second: %c \n", *Var1);
+
+    *Var1 = 'E';
+    *Var2 = 'a';
+
+    printf("After: %s \n", content);
+    char content1[100] = "AAtions speak louder than words";
+    changeSecondLetter(content1);
+    printf("Fixed: %s\n", content1);
 
     return 0;
 }
