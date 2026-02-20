6643f4d8e99cf833e35aa312a44bebef5d0ecbe8
daniel.ham <hamburger.danie-it25@it.dhbw-ravensburg.de>
2026-02-20 12:34:43 +0100

    Section 3 start

--- courses/coding-in-C/SolutionDaniel/Lab-6/Section3.c
+++ courses/coding-in-C/SolutionDaniel/Lab-6/Section3.c
@@ -11,14 +11,21 @@ struct Sensor{
     int object_detection[3000];
 };
 
-
 int main(){
     FILE *f;
     f = fopen("sensor1.txt", "r");
     if (!f){
     return 1;
     } 
+    
+    float time=0;
+    float probability=0;
+    for (int i = 0; i < 3000; i++){
+        fscanf(f, "%f %lf", &time, &probability);
 
+        printf("%.2f %.2lf\n", time , probability);
+    }
+  
     int time1 = fgetc(f);
     printf("%i\n", time1);
 
